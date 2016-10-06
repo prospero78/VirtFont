75735723f3b6587b657a01b5d71f9309aac366f4
prospero78 <prospero.78.su@gmail.com>
2016-10-06 17:03:54 +0200

    02 Main module file

---
+++ VirtFont.pas
@@ -0,0 +1,12 @@
+library VirtFont;
+
+var
+   ///- נוזטל נאבמעû רנטפעא
+   /// 0: 8x8 עמקוך
+   mode: integer := 0;
+   En_A: ARRAY [1..8] OF BYTE :=  (0, 112, 72, 68, 66, 66, 126, 66);
+   En_B: ARRAY [1..8] OF BYTE :=  (0, 30, 34, 34, 66, 66, 66, 62);
+   En_C: ARRAY [1..8] OF BYTE :=  (0, 60, 66, 2, 2, 2, 66, 60);
+   En_D: ARRAY [1..8] OF BYTE :=  (0, 60, 66, 2, 2, 2, 66, 60);
+
+end.
